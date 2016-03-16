9c102f76207303ccd6daa938c63a561742a96094
HubertLegec <hubert.legec@gmail.com>
2016-03-16 01:13:07 +0100

    android project structure, android annotations works

---
+++ app/src/main/java/com/legec/imgsearch/app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.legec.imgsearch.app;
+
+import android.app.Activity;
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends Activity {
+
+    @AfterViews
+    public void initViews(){
+
+    }
+}
