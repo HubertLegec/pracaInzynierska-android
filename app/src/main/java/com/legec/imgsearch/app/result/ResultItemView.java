1ae70ac80aba5d116ef4e0071933f6dd3f1c47de
HubertLegec <hubert.legec@gmail.com>
2017-01-15 14:08:13 +0100

    result list restyling

--- app/src/main/java/com/legec/imgsearch/app/result/ResultItemView.java
+++ app/src/main/java/com/legec/imgsearch/app/result/ResultItemView.java
@@ -1,6 +1,10 @@
 package com.legec.imgsearch.app.result;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
@@ -11,12 +15,13 @@ import com.legec.imgsearch.app.restConnection.dto.ImageDetails;
 
 import org.androidannotations.annotations.EViewGroup;
 import org.androidannotations.annotations.ViewById;
+import org.apache.commons.lang3.StringUtils;
 
 
 @EViewGroup(R.layout.result_element)
 public class ResultItemView extends LinearLayout {
     @ViewById
-    TextView matchRatio;
+    Button webPageButton;
     @ViewById
     TextView name;
     @ViewById
@@ -24,13 +29,28 @@ public class ResultItemView extends LinearLayout {
     @ViewById
     ProgressBar progress;
 
+    private final Context context;
+
 
     public ResultItemView(Context context) {
         super(context);
+        this.context = context;
     }
 
     public void bind(ImageDetails entry) {
-        name.setText("Name: " + entry.getName());
-        matchRatio.setText("Match rate: " + String.format("%.3f", entry.getMatchRate()));
+        name.setText(entry.getName());
+        final String url = entry.getPageUrl();
+        if (StringUtils.isBlank(url)) {
+            webPageButton.setEnabled(false);
+        } else {
+            webPageButton.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    context.startActivity(intent);
+                }
+            });
+        }
     }
 }
